c9b4f201d62840eabffff53a5c12533306bdc56a
Chandrahas Tirumalasetty <chandrahas@main-10-231-175-227.tamulink.tamu.edu>
2018-09-09 21:06:54 -0500

    Added ContFramepool and needed_info_frame functions

--- MP2/MP2_Sources/cont_frame_pool.C
+++ MP2/MP2_Sources/cont_frame_pool.C
@@ -1,91 +1,91 @@
 /*
  File: ContFramePool.C
- 
+
  Author:
- Date  : 
- 
+ Date  :
+
  */
 
 /*--------------------------------------------------------------------------*/
-/* 
+/*
  POSSIBLE IMPLEMENTATION
  -----------------------
 
  The class SimpleFramePool in file "simple_frame_pool.H/C" describes an
- incomplete vanilla implementation of a frame pool that allocates 
- *single* frames at a time. Because it does allocate one frame at a time, 
+ incomplete vanilla implementation of a frame pool that allocates
+ *single* frames at a time. Because it does allocate one frame at a time,
  it does not guarantee that a sequence of frames is allocated contiguously.
  This can cause problems.
- 
+
  The class ContFramePool has the ability to allocate either single frames,
  or sequences of contiguous frames. This affects how we manage the
- free frames. In SimpleFramePool it is sufficient to maintain the free 
+ free frames. In SimpleFramePool it is sufficient to maintain the free
  frames.
  In ContFramePool we need to maintain free *sequences* of frames.
- 
- This can be done in many ways, ranging from extensions to bitmaps to 
+
+ This can be done in many ways, ranging from extensions to bitmaps to
  free-lists of frames etc.
- 
+
  IMPLEMENTATION:
- 
+
  One simple way to manage sequences of free frames is to add a minor
  extension to the bitmap idea of SimpleFramePool: Instead of maintaining
- whether a frame is FREE or ALLOCATED, which requires one bit per frame, 
+ whether a frame is FREE or ALLOCATED, which requires one bit per frame,
  we maintain whether the frame is FREE, or ALLOCATED, or HEAD-OF-SEQUENCE.
- The meaning of FREE is the same as in SimpleFramePool. 
+ The meaning of FREE is the same as in SimpleFramePool.
  If a frame is marked as HEAD-OF-SEQUENCE, this means that it is allocated
  and that it is the first such frame in a sequence of frames. Allocated
  frames that are not first in a sequence are marked as ALLOCATED.
- 
- NOTE: If we use this scheme to allocate only single frames, then all 
+
+ NOTE: If we use this scheme to allocate only single frames, then all
  frames are marked as either FREE or HEAD-OF-SEQUENCE.
- 
- NOTE: In SimpleFramePool we needed only one bit to store the state of 
+
+ NOTE: In SimpleFramePool we needed only one bit to store the state of
  each frame. Now we need two bits. In a first implementation you can choose
  to use one char per frame. This will allow you to check for a given status
- without having to do bit manipulations. Once you get this to work, 
- revisit the implementation and change it to using two bits. You will get 
+ without having to do bit manipulations. Once you get this to work,
+ revisit the implementation and change it to using two bits. You will get
  an efficiency penalty if you use one char (i.e., 8 bits) per frame when
  two bits do the trick.
- 
+
  DETAILED IMPLEMENTATION:
- 
+
  How can we use the HEAD-OF-SEQUENCE state to implement a contiguous
  allocator? Let's look a the individual functions:
- 
- Constructor: Initialize all frames to FREE, except for any frames that you 
+
+ Constructor: Initialize all frames to FREE, except for any frames that you
  need for the management of the frame pool, if any.
- 
- get_frames(_n_frames): Traverse the "bitmap" of states and look for a 
- sequence of at least _n_frames entries that are FREE. If you find one, 
+
+ get_frames(_n_frames): Traverse the "bitmap" of states and look for a
+ sequence of at least _n_frames entries that are FREE. If you find one,
  mark the first one as HEAD-OF-SEQUENCE and the remaining _n_frames-1 as
  ALLOCATED.
 
  release_frames(_first_frame_no): Check whether the first frame is marked as
  HEAD-OF-SEQUENCE. If not, something went wrong. If it is, mark it as FREE.
- Traverse the subsequent frames until you reach one that is FREE or 
+ Traverse the subsequent frames until you reach one that is FREE or
  HEAD-OF-SEQUENCE. Until then, mark the frames that you traverse as FREE.
- 
+
  mark_inaccessible(_base_frame_no, _n_frames): This is no different than
  get_frames, without having to search for the free sequence. You tell the
  allocator exactly which frame to mark as HEAD-OF-SEQUENCE and how many
  frames after that to mark as ALLOCATED.
- 
- needed_info_frames(_n_frames): This depends on how many bits you need 
+
+ needed_info_frames(_n_frames): This depends on how many bits you need
  to store the state of each frame. If you use a char to represent the state
  of a frame, then you need one info frame for each FRAME_SIZE frames.
- 
+
  A WORD ABOUT RELEASE_FRAMES():
- 
+
  When we releae a frame, we only know its frame number. At the time
  of a frame's release, we don't know necessarily which pool it came
- from. Therefore, the function "release_frame" is static, i.e., 
+ from. Therefore, the function "release_frame" is static, i.e.,
  not associated with a particular frame pool.
- 
+
  This problem is related to the lack of a so-called "placement delete" in
  C++. For a discussion of this see Stroustrup's FAQ:
  http://www.stroustrup.com/bs_faq2.html#placement-delete
- 
+
  */
 /*--------------------------------------------------------------------------*/
 
@@ -133,7 +133,53 @@ ContFramePool::ContFramePool(unsigned long _base_frame_no,
                              unsigned long _n_info_frames)
 {
     // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    // Do Assertion if number of information frames supplied exceeds the number of information frames required
+    assert(_n_info_frames >= needed_info_frames(_n_frames));
+
+    // Copy the inputs to local variables
+    base_frame_no = _base_frame_no;
+    n_frames = _n_frames;
+    n_free_frames = _n_frames;
+    info_frame_no = _info_frame_no;
+    n_info_frames = _n_info_frames;
+
+    // Check the info_frame_no to decide where to place the management information
+    if(info_frame_no == 0)
+    {
+      // Management info should be stored internally in the first frame
+      ext_bitmap = (unsigned char*)(base_frame_no * n_info_frames * FRAME_SIZE);
+    }
+    else
+    {
+      // Management info should be stored in the given frame number in given number of information frames
+      ext_bitmap = (unsigned char*)(info_frame_no * n_info_frames * FRAME_SIZE);
+    }
+
+    // Number of frames must be able to fill the bitmap
+    //assert ((n_frames % 8) == 0);
+
+    // Proceed to mark in bitmap that all frames are allocated
+    for(int i = 0; i < n_frames; i=i+1)
+    {
+      ext_bitmap[i] = INFO_FREE_FRAME;
+    }
+
+    // if you are using frames with in the pool for information, mark them as used
+    if(info_frame_no == 0)
+    {
+      for(i=0;i<n_info_frames;i++)
+      {
+        ext_bitmap[i] = INFO_ALLOCATED_FRAME;
+        n_free_frames--;
+      }
+    }
+
+    // The very first frame in the pool becomes the head of the frames
+    // verify this
+    ext_bitmap[0] = INFO_HEAD_FRAME;
+
+    Console::puts("Frame Pool initialized\n");
+
 }
 
 unsigned long ContFramePool::get_frames(unsigned int _n_frames)
@@ -158,5 +204,8 @@ void ContFramePool::release_frames(unsigned long _first_frame_no)
 unsigned long ContFramePool::needed_info_frames(unsigned long _n_frames)
 {
     // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    // Caluculate how many frame information can be stored in a frame
+    unsigned int frame_info_capacity =(8*(FRAME_SIZE))/INFO_BITS_PER_FRAME;
+    return (_n_frames/frame_info_capacity + (_n_frames % frame_info_capacity > 0 ? 1 : 0));
+
 }
